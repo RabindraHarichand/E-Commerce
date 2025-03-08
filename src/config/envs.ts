c8e4bc960174512423c7e8d7aec07dd4aa7c9d17
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-08 09:44:22 -0400

    [config]: Add config floder with envs file to validate .env variables

---
+++ src/config/envs.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import * as zod from "zod";
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = zod
+  .object({
+    PORT: zod.coerce.number(),
+  })
+  .strict()
+  .passthrough();
+
+const { error, data } = envsSchema.safeParse(process.env);
+
+console.log({ error, data });
+
+// if (error) {
+//   throw new Error(`Config validation error: ${error.message}`);
+// }
+
+// const envVars: EnvVars = data;
+
+// export const envs = {
+//   port: envVars.PORT,
+// };
